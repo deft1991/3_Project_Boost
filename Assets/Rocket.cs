7ffd3805d71216ebcac6d2d2a752c0103fd21e81
deft <deft1991@gmail.com>
2020-10-18 21:33:12 +0300

    Add sound& Fix rotation

---
+++ Assets/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessRemoteInput();
+    }
+
+    private void ProcessRemoteInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            _rigidbody.AddRelativeForce(Vector3.up);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}

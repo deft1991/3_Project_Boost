cdbc0b2bb7fc008e8a2d8748fd83d6f8ff1011d0
deft <deft1991@gmail.com>
2020-10-19 18:41:32 +0300

    Add blocks and serialized fields

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,8 @@ public class Rocket : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private AudioSource _audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] private float thrustThisFrame = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +26,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) // can thrust while rotating
         {
-            _rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!_audioSource.isPlaying)
             {
                 _audioSource.Play();
@@ -39,14 +41,16 @@ public class Rocket : MonoBehaviour
     private void Rotate()
     {
         _rigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = Time.deltaTime * rcsThrust;
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
 
         _rigidbody.freezeRotation = false; // resume physics control of rotation 
